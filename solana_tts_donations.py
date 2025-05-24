800d352a26a67d0382d4c1b4e68f9d63a6a2fd21
1337 <a1337@1337s-MacBook-Pro-2.local>
2025-05-24 07:54:48 +0200

    shit

--- solana_tts_donations.py
+++ solana_tts_donations.py
@@ -111,4 +111,5 @@ def home():
 if __name__ == "__main__":
     from threading import Thread
     Thread(target=monitor_wallet, daemon=True).start()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
